9715bae3b4816775529ff0738479bbcb4bf035ca
Caleb <CalebOttoGameDev@Gmail.com>
2022-06-20 23:46:29 +1000

    main: Move code out of main

--- main.go
+++ main.go
@@ -1,49 +1,6 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-	"tiny/analysis"
-	"tiny/parser"
-	"tiny/runtime"
-	"tiny/shared"
-)
-
 func main() {
-	var (
-		checkOnly bool
-		// usevm     bool
-		script string
-	)
-
-	flag.BoolVar(&checkOnly, "check", false, "Checks if code is valid and does not run")
-	// flag.BoolVar(&usevm, "vm", false, "Use the new interpreter to run code")
-	flag.StringVar(&script, "script", "", "Script to run")
-	flag.Parse()
-
-	if len(script) == 0 {
-		fmt.Println("usage: tiny [-script][-check]")
-		return
-	}
-
-	if source, ok := shared.ReadFileErr(script); ok {
-
-		parser := parser.New(source, script)
-		program := parser.Parse()
-		analyser := analysis.NewAnalyser(false)
-
-		if !analyser.Run(program.Body) {
-			return
-		}
-
-		if !checkOnly {
-			interpreter := runtime.New()
-			interpreter.Run(program)
-		} else {
-			fmt.Println("Good!")
-		}
-	} else {
-		shared.ReportErrFatal(fmt.Sprintf("File '%s' does not exist.", os.Args[1]))
-	}
+	tiny := New()
+	tiny.Run()
 }
